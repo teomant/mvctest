fa3c3a1ecd2376527a4b84490a5b8cc5bac1bc4d
teomant <teomant@bk.ru>
2019-03-02 17:42:19 +0400

    soap server

--- src/main/java/org/teomant/config/Initializer.java
+++ src/main/java/org/teomant/config/Initializer.java
@@ -3,7 +3,7 @@ package org.teomant.config;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
@@ -14,14 +14,16 @@ public class Initializer implements WebApplicationInitializer{
     @Override
     public void onStartup( ServletContext servletContext ){
         AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-        ctx.register( WebAppConfig.class, CountryConfiguration.class );
+        ctx.register( WebServiceConfig.class);
         servletContext.addListener( new ContextLoaderListener( ctx ) );
 
         ctx.setServletContext( servletContext );
 
-        ServletRegistration.Dynamic servlet =
-                servletContext.addServlet( "dispatcher" , new DispatcherServlet( ctx ) );
-        servlet.addMapping( "/" );
-        servlet.setLoadOnStartup( 1 );
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(ctx);
+        servlet.setTransformWsdlLocations(true);
+        ServletRegistration.Dynamic dynamic = servletContext.addServlet("dispatcher",servlet);
+        dynamic.addMapping("/*");
+        dynamic.setLoadOnStartup(1);
     }
 }
